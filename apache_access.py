da2367e4c63d408e671cacf4666d601b0da73303
松島　佑樹 <matsuhima@MatsushiMac.local>
2018-04-06 17:29:42 +0900

    17:29

--- apache_access.py
+++ apache_access.py
@@ -9,7 +9,9 @@ def sorted_list(l):
     dict ={}
     for member in s:
         dict[member] = l.count(member)
-    for i,j in sorted(dict.items()):
+    
+    print("hostname : access")
+    for i,j in sorted(dict.items(),key=lambda x: -x[1]):
         print(str(i)+":"+str(j))
     return 0
 def date_to_epoch(d):
@@ -20,9 +22,10 @@ if __name__=='__main__':
     parser.add_argument("-l",
                         "--log",
                         nargs="*",
+                        default=["/var/log/httpd/access_log"],
                         type=str,
-                        required=True,
-                        help=u"ログファイルのパスを指定してください。",
+                        #required=True,
+                        help=u"ログファイルのパスを指定してください。 | 初期値: %(default)",
                         dest="input_logs"
                         )
     parser.add_argument("-f",
@@ -36,13 +39,13 @@ if __name__=='__main__':
     parser.add_argument("-t",
                         "--to",
                         nargs="?",
-                        default="4000-12-31",
+                        default="9999-12-31",
                         type=str,
                         help=u"対象期間の終了日をY-m-dの形式で指定してください。 | 初期値: %(default)s",
                         dest="end_date"
                        )
     stdate = parser.parse_args().start_date+"-00:00:00"
-    endate = parser.parse_args().end_date+"-00:00:00"
+    endate = parser.parse_args().end_date+"-23:59:59"
 
     date_from = date_to_epoch(datetime.datetime.strptime(stdate, "%Y-%m-%d-%H:%M:%S"))
     date_to = date_to_epoch(datetime.datetime.strptime(endate, "%Y-%m-%d-%H:%M:%S"))
@@ -53,34 +56,48 @@ if __name__=='__main__':
     acc = {}
     for hour in range(24):
         acc[hour] = 0
-
     argvs = parser.parse_args().input_logs
-    for filename in argvs:
-        f = open(filename)
-        for line in f.readlines():
-            line = line.strip()
-            stat_list = line.split("\"")
 
-            date = stat_list[0].split()[3][1:]
-            tdatetime = datetime.datetime.strptime(date,'%d/%b/%Y:%H:%M:%S')
-            if((date_from>date_to_epoch(tdatetime))|(date_to<date_to_epoch(tdatetime))):
-                continue
-            
-            acc[tdatetime.hour] += 1
-            hostname = stat_list[0].split()[0]
-            host_list.append(hostname)
-            
-            client_id = stat_list[0].split()[1]
-            usr = stat_list[0].split()[2]
-            zone = stat_list[0].split()[4][:-1]
-            first_line = stat_list[1].strip()
-            response_stat = stat_list[2].split()[0]
-            response_bite = stat_list[2].split()[1]
-            referer = stat_list[3].strip()
-            UA = stat_list[5].strip()
+
+    for filename in argvs:
+        try:
+            f = open(filename)
+        except IOError:
+            print ('"%s" cannnot be opened.' % filename)
+
+        else:
+            for line in f.readlines():
+                line = line.strip()
+                stat_list = line.split("\"")
+
+                date = stat_list[0].split()[3][1:]
+                tdatetime = datetime.datetime.strptime(date,'%d/%b/%Y:%H:%M:%S')
+                if((date_from>date_to_epoch(tdatetime))|(date_to<date_to_epoch(tdatetime))):
+                    continue
+                
+                acc[tdatetime.hour] += 1
+                hostname = stat_list[0].split()[0]
+                host_list.append(hostname)
+                
+                client_id = stat_list[0].split()[1]
+                usr = stat_list[0].split()[2]
+                zone = stat_list[0].split()[4][:-1]
+                first_line = stat_list[1].strip()
+                response_stat = stat_list[2].split()[0]
+                response_bite = stat_list[2].split()[1]
+                referer = stat_list[3].strip()
+                UA = stat_list[5].strip()
         
-        f.close()
-    #
+            f.close()
+    
     sorted_list(host_list)
-    for i,j in acc.items():
-        print(str(i)+":"+str(j))
+
+    print('{:>20}'.format("Hour")),
+    for i in acc.keys():
+        print("|"+'{:>5}'.format(str(i)+"-"+str(i+1))),
+
+    print("|")
+    print('{:>20}'.format("Number of acccesses")),
+    for i in acc.keys():
+        print("|"+'{:>5}'.format(str(acc[i]))),
+    print("|")
